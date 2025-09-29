3e80f6f8fb0a962e012f914dea472a0a2d5843e0
syjang <jsy2848@gmail.com>
2025-09-29 10:44:39 +0900

    use my robots.txt

--- app/src/app/robots.ts
+++ app/src/app/robots.ts
@@ -4,10 +4,25 @@ import { getSiteUrl } from '@/lib/site';
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = getSiteUrl();
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: [
+          'Amazonbot',
+          'Applebot-Extended',
+          'Bytespider',
+          'CCBot',
+          'ClaudeBot',
+          'Google-Extended',
+          'GPTBot',
+          'meta-externalagent',
+        ],
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
